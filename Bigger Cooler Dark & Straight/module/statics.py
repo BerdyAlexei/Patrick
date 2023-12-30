9432799f1d0a8adc8a1faf938d4a0dffc6d7bb06
Berdy Alexei <108490702+BerdyAlexei@users.noreply.github.com>
2023-12-30 12:22:18 -0500

    The modernized Patrick look with the UI enlarged
    The alternative Patrick look but with the UI sizes multiplied by one-dot-one (1.1) as the static variable MULTIPLIER in statics.py indicates, of course modifiable from there.

---
+++ Bigger Cooler Dark & Straight/module/statics.py
@@ -0,0 +1,26 @@
+PATH = '.'
+
+DATA = PATH + '/data'
+
+RESOURCE = DATA + '/resource'
+CONFIGURATION = DATA + '/configuration/{}.json'
+
+FILE = RESOURCE + '/file/{}'
+LANG = RESOURCE + '/lang/{}.json'
+FONT = RESOURCE + '/font/{}.ttf'
+SVG  = RESOURCE + '/svg/{}.svg'
+
+FONT_SIZE = 11
+
+NAME   = 'Patrick'
+WIDTH  = 500
+HEIGHT = 312
+
+WIDGET_HEIGHT = 32
+WIDGET_WIDTH  = 32
+WIDGET_SPACE  = 8
+
+GRAB_WIDTH  = 404
+GRAB_HEIGHT = 32
+
+MULTIPLIER = 1.1
